3bdc3a33381481b97a1369a5235d435eee10c692
megazear7 <alexlockhart7@gmail.com>
2014-02-21 12:44:09 -0500

    question fo jim

---
+++ question_for_jim/example.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+void a() {
+	printf("hello");
+}
+
+void b() {
+	printf("world");
+}
+
+
+int main() {
+
+	// is there some thing I can do here such as:
+	a=&b; // <- note this does note compile / work but captures the idea of my goal 
+
+	// that will make the next line print "world"
+	a();
+
+	return 0;
+}
